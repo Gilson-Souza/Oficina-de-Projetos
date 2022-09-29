ecb505445664646d741470487f234239ee7821b0
gilson-souza <gilsonpro10@gmail.com>
2022-09-29 00:37:42 -0300

    atualização add projeto

---
+++ maquina-de-vendas/produtos.js
@@ -0,0 +1,29 @@
+let produtos = [
+
+    {
+        nome: 'jujuba',
+        valor: '5.30',
+        estoque: '20',
+    },
+    {
+        nome: 'jurubeba',
+        valor: '2.30',
+        estoque: '10',
+    },
+    {
+        nome: 'ameixa',
+        valor: '2.32',
+        estoque: '10',
+    },
+    {
+        nome: 'pereba',
+        valor: '10.30',
+        estoque: '10',
+    },
+    {
+        nome: 'bala',
+        valor: '1.10',
+        estoque: '8',
+    },
+
+];
---
+++ maquina-de-vendas/script.js
@@ -0,0 +1,51 @@
+const produtos = document.querySelector('.item-prod');
+const setaBaixo = document.querySelector('.seta-baixo');
+const setaCima = document.querySelector('.seta-cima');
+const display = document.querySelectorAll('.item-display');
+const comprar = document.querySelector('.comprar');
+const saldo = document.querySelector('.item-sal-tela');
+const excluir = document.querySelector('#excluir');
+const confirmar = document.querySelector('#confirmar');
+const retirar = document.querySelector('.item-sal');
+const botoes = document.querySelectorAll('.botao');
+
+
+let numero = '';
+
+let cliente = [
+    {
+        saldo_d:'',
+        credito:'', 
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
